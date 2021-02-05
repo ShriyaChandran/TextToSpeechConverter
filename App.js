126097ad2d6ce9ef59b8ab666d7e9e0e40c95610
ShriyaChandran <62100109+ShriyaChandran@users.noreply.github.com>
2021-02-05 20:54:12 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -12,21 +12,40 @@ export default class App extends React.Component{
   render(){
     return(
       <View>
-        <Text>Please type in the text you want as speech.</Text>
+        <Text style={styles.container}>Please type in the text you want as speech.</Text>
         <TextInput 
         value= {this.state.text}
         onChangeText={(text)=>{
           this.setState({text: text})
-        }}/>
+        }}
+        style={{width:200, height:50, borderWidth:1}}
+        placeholder="Type Here"
+        />
         <TouchableOpacity onPress={()=>{
           this.speak(this.state.text)
         }}>
-          <Text> Text to speech </Text>
+          <Text style={styles.container}> Text to speech </Text>
         </TouchableOpacity>
       </View>
     )
   }
   speak=()=>{
-    Speech.speak(this.state.text);
+    this.state.text= ("")?alert("This text is invalid"):Speech.speak(this.state.text);
   }
 }
+
+
+const styles= StyleSheet.create({
+  container:{
+    flex: 1,
+    fontSize:18,
+    marginTop:150,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text:{
+    fontSize: 24,
+    textDecorationLine: 'underline',
+    justifyContent: 'center'
+  }
+})
